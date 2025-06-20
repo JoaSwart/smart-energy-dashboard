3352d3a94819a74a916390970653c1b832678570
JoaSwart <leentjezz@outlook.com>
2025-06-20 14:20:58 +0200

    comments bij de javascript van het huisverbruik toegevoegd

--- huisverbruik.js
+++ huisverbruik.js
@@ -1,21 +1,21 @@
 console.log("script.js werkt");
 
 fetch('../data/huisverbruik.csv')
-  .then(response => response.text())
+  .then(response => response.text()) //omgezet naar text
   .then(csvText => {
     
-    const lines = csvText.trim().split('\n');
-    lines.shift(); // remove header
+    const lines = csvText.trim().split('\n'); //verwijder onnodige regels
+    lines.shift(); // verwijder de eerste regels van het csv bestand (tijd, verbruik)
 
-    const labels = [];
-    const values = [];
+    const labels = []; //tijd
+    const values = []; //verbruik
 
     lines.forEach(line => {
       // . of , ertussen
       const parts = line.split(/\t|,/);
-      if (parts.length >= 2) {
-        labels.push(parts[0].trim());
-        values.push(parseFloat(parts[1].trim()));
+      if (parts.length >= 2) { //als er minstens 2 stukjes data zijn 
+        labels.push(parts[0].trim()); //gaat het eerste stukje naar labels
+        values.push(parseFloat(parts[1].trim())); //parsefloat zet het om naar kommagetallen ipv text
       }
     });
 
@@ -32,9 +32,8 @@ fetch('../data/huisverbruik.csv')
           backgroundColor: 'rgba(56, 161, 105, 0.10)',
           borderWidth: 2,
           pointRadius: 0,
-          tension: 0.4,
+          tension: 0.4, //hoe gebogen of hoekig de lijn is
           fill: true,
-          borderCapStyle: 'round',
         }]
       },
       options: {
